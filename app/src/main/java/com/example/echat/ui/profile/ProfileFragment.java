31d5786f409293b1db8a8bdfff35c949d6ee0579
=allenbangai <=allenkamadje@gmail.com>
2020-09-18 02:06:29 +0100

    refactoring to create profile page

---
+++ app/src/main/java/com/example/echat/ui/profile/ProfileFragment.java
@@ -0,0 +1,25 @@
+package com.example.echat.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.echat.R;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel profileViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        profileViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        return root;
+    }
+}
