033117e370e24b7505d83fe88762d3fedfb8c821
=allenbangai <=allenkamadje@gmail.com>
2020-09-18 02:12:56 +0100

    declaring paramters for profile page

--- app/src/main/java/com/example/echat/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/echat/ui/profile/ProfileFragment.java
@@ -10,16 +10,30 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.echat.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+
+    private DatabaseReference databaseReference;
+    private FirebaseUser user;
+    private StorageReference storageReference;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        storageReference = FIrebaseStorage.getInstance().getReference().child(user.getUid());
+
+
         return root;
     }
 }
