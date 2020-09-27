b5465dbfe47361ffae8993c72c5cd6ff534be0ec
=allenbangai <=allenkamadje@gmail.com>
2020-09-27 01:27:41 +0100

    profile fragment xml

--- app/src/main/java/com/example/echat/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/echat/ui/profile/ProfileFragment.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.echat.Model.User;
 import com.example.echat.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileFragment extends Fragment {
+
+    private CircleImageView profileImage, saveImage, editImage;
+    private TextView profileEmail, profileUserName, profileNumber;
+    private EditText editProfileEmail, editProfileUsername, editProfileNumber;
 
     private ProfileViewModel profileViewModel;
 
@@ -31,9 +45,31 @@ public class ProfileFragment extends Fragment {
 
         databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
         user = FirebaseAuth.getInstance().getCurrentUser();
-        storageReference = FIrebaseStorage.getInstance().getReference().child(user.getUid());
+        storageReference = FirebaseStorage.getInstance().getReference().child(user.getUid());
+
+        profileImage = root.findViewById(R.id.profile_image);
+        saveImage = root.findViewById(R.id.save_profile_info);
+        editImage = root.findViewById(R.id.edit_profile_info);
+
+
 
 
         return root;
+    }
+
+    private void loadProfileInfo(){
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
